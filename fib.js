3409fabdea36455ac8ac69533a5db6736090f596
lavantien <lavantien96@gmail.com>
2023-06-06 01:09:20 +0700

    use es module

--- fib.js
+++ fib.js
@@ -1,21 +1,21 @@
-const { workerData, parentPort } = require('worker_threads')
+import { workerData, parentPort } from 'worker_threads'
 
 function fib(x) {
-	if (x < 1n) {
-		return 0n
-	}
-	if (x <= 2n) {
-		return 1n
-	}
-	let a = 1n
-	let b = 1n
-	let c = 0n
-	for (let i = 3n; i <= x; i++) {
-		c = a + b
-		a = b
-		b = c
-	}
-	return c
+  if (x < 1n) {
+    return 0n
+  }
+  if (x <= 2n) {
+    return 1n
+  }
+  let a = 1n
+  let b = 1n
+  let c = 0n
+  for (let i = 3n; i <= x; i++) {
+    c = a + b
+    a = b
+    b = c
+  }
+  return c
 }
 
 const result = fib(workerData.data)
--- main.js
+++ main.js
@@ -1,44 +1,44 @@
-const { Worker } = require('worker_threads')
+import { Worker } from 'worker_threads'
 
 const n = 1000000
 const numberOfWorkers = 16
 
 const doFib = async (n) => {
-	return new Promise((resolve, reject) => {
-		const startTime = Date.now()
-		const data = BigInt(n)
+  return new Promise((resolve, reject) => {
+    const startTime = Date.now()
+    const data = BigInt(n)
 
-		const worker = new Worker('./fib.js', {
-			workerData: {
-				data,
-			},
-		})
+    const worker = new Worker('./fib.js', {
+      workerData: {
+        data,
+      },
+    })
 
-		worker.once('message', (message) => {
-			console.log(
-				`worker [${worker.threadId}] took ${Date.now() - startTime}ms`,
-			)
-			resolve(message)
-		})
-		worker.once('error', (error) => {
-			reject(error)
-		})
-	})
+    worker.once('message', (message) => {
+      console.log(
+        `worker [${worker.threadId}] took ${Date.now() - startTime}ms`,
+      )
+      resolve(message)
+    })
+    worker.once('error', (error) => {
+      reject(error)
+    })
+  })
 }
 
 const main = async () => {
-	try {
-		const actions = []
-		for (let i = 0; i < numberOfWorkers; i++) {
-			actions.push(doFib(n))
-		}
-		const startTime = Date.now()
-		const values = await Promise.all(actions)
-		const digitsCount = values.map((value) => value.toString().length)
-		console.log('values: ', digitsCount)
-		console.log(`all took ${Date.now() - startTime}ms`)
-	} catch (err) {
-		console.error(err)
-	}
+  try {
+    const actions = []
+    for (let i = 0; i < numberOfWorkers; i++) {
+      actions.push(doFib(n))
+    }
+    const startTime = Date.now()
+    const values = await Promise.all(actions)
+    const digitsCount = values.map((value) => value.toString().length)
+    console.log('values: ', digitsCount)
+    console.log(`all took ${Date.now() - startTime}ms`)
+  } catch (err) {
+    console.error(err)
+  }
 }
 main().catch(console.error)
